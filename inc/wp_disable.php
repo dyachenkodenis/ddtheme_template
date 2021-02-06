a8075507bb5202d815a36bdfb4e15f8fe495aede
dyachenkodenis <denisdyachenko.90@gmail.com>
2021-02-06 18:18:54 +0500

    добавил filter и action блокировки в wp_head

--- inc/wp_disable.php
+++ inc/wp_disable.php
@@ -8,22 +8,14 @@
  * @license     
  */
 
-
-/**
- * Disable Emoji support introduced in WP 4.2
- */
-/*function pss_disable_emoji() {
-
-    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-    remove_action( 'wp_print_styles', 'print_emoji_styles' );
-    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-    remove_action( 'admin_print_styles', 'print_emoji_styles' );
-    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-
-}
-add_action( 'init', 'pss_disable_emoji' );
+  //Disable Emoji
+  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+  remove_action( 'wp_print_styles', 'print_emoji_styles' );
+  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+  remove_action( 'admin_print_styles', 'print_emoji_styles' );
+  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
 
   // Remove really simple discovery link
   remove_action('wp_head', 'rsd_link');
@@ -44,4 +36,23 @@ add_action( 'init', 'pss_disable_emoji' );
   remove_action('wp_head', 'wp_oembed_add_discovery_links', 10);
 
   // Disable REST API link in HTTP headers
-  remove_action('template_redirect', 'rest_output_link_header', 11, 0);*/
+  remove_action('template_redirect', 'rest_output_link_header', 11, 0);
+
+  remove_action('wp_head', 'start_post_rel_link');
+
+  remove_action('wp_head', 'index_rel_link');
+
+  remove_action('wp_head', 'adjacent_posts_rel_link');
+
+  add_filter( 'feed_links_show_comments_feed', '__return_false' );
+
+  remove_action( 'wp_head', 'feed_links' );
+
+  function remove_comments_rss( $for_comments ) {
+    return;
+  }
+  add_filter('post_comments_feed_link','remove_comments_rss');
+
+  remove_action('wp_head', 'rel_canonical');
+  
+  remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
