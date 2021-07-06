cbec8235377caeaf2a785421817f21d5fa5e9059
dyachenkodenis <denisdyachenko.90@gmail.com>
2021-07-06 17:03:50 +0500

    add 404 page

---
+++ 404.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Wordpress Theme
+* Template Name: 404 Page
+* @version     1.0.0
+* @author
+* @link
+* @copyright   2021 Wordpress Theme
+* @license
+* @package WordPress
+* @subpackage Wordpress Theme
+* @since Wordpress Theme 1.0
+*/
+if (!defined('ABSPATH')) { die(); }
+?>
+<?php get_header(); ?>
+
+<h1><?php echo "404"; ?></h1>
+
+<?php get_footer(); ?>
